e0791d813c481d99c42f3082fab95e0d7ca0585d
Federico Bond <federicobond@gmail.com>
2016-02-15 14:03:06 -0300

    Refactor main into two functions

--- src/Main.hs
+++ src/Main.hs
@@ -4,29 +4,34 @@ import Text.Parsec
 import Text.Parsec.Error
 import NgLint.Parser
 import NgLint.Linter
+import NgLint.Matchers
 
 
 printUsage :: IO ()
 printUsage = putStrLn "usage: nglint file.conf"
 
 
+lintFile :: FilePath -> IO [LintMessage]
+lintFile fileName = do
+    content <- readFile fileName
+    let config = parse configFile fileName content
+    case config of
+        Left error -> do
+            print error
+            return []
+        Right (Config decls) -> do
+            mapM_ (printLintMessage content) messages
+            return messages
+            where messages = lint decls
+
+
+main :: IO ()
 main = do
     params <- getArgs
     case length params of
         0 -> printUsage
-        _ -> do 
-            let fileName = head params
-            content <- readFile fileName
-
-            let config = parse configFile fileName content
-            case config of
-                Left error -> do
-                    print error
-                    exitFailure
-                Right (Config decls) ->
-                    if null messages
-                        then exitSuccess
-                        else do
-                            mapM_ (printLintMessage content) messages
-                            exitFailure
-                    where messages = lint decls
+        _ -> do
+            totalMessages <- mapM lintFile params
+            let num = length $ concat totalMessages
+            putStrLn $ show num ++ " hints."
+            if num > 0 then exitFailure else exitSuccess
